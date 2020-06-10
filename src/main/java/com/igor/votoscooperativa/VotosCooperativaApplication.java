849d87b75f4b3c6dffe150357a68848b63e8b468
Igor Trusz <trusz.igor@gmail.com>
2020-06-10 01:01:06 -0300

    Primeiro commit

---
+++ src/main/java/com/igor/votoscooperativa/VotosCooperativaApplication.java
@@ -0,0 +1,13 @@
+package com.igor.votoscooperativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotosCooperativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotosCooperativaApplication.class, args);
+	}
+
+}
